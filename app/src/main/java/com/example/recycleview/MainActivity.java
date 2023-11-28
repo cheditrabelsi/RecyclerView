52397508ea5271224158f0e57a9de8c748f56a15
cheditrabelsi <cheditrabelsi822@gmail.com>
2023-11-28 22:29:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private com.example.recycleview.WordListAdapter mAdapter;
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + mCount++);
+            Log.d("WordList", mWordList.getLast());
+        }
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mAdapter = new com.example.recycleview.WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                mWordList.addLast("+ Word " + wordListSize);
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/recycleview/WordListAdapter.java
@@ -0,0 +1,74 @@
+package com.example.recycleview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder>{
+
+
+
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+
+    }
+
+
+
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener
+    {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        @Override
+        public void onClick(View v) {
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition, "Clicked! " + element);
+            mAdapter.notifyDataSetChanged();
+        }
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = (TextView) itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+    }
+
+
+
+}
